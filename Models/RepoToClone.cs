67ba38fc0aaa59eeb7ab6b71edd788cdbab7fcf6
Paul Ikeda <paul.ikeda@gmail.com>
2019-11-22 21:43:14 -0800

    Support for cloning multiple repos

---
+++ Models/RepoToClone.cs
@@ -0,0 +1,9 @@
+﻿namespace RepoManager.Models
+{
+    public class RepoToClone
+    {
+        public string RemoteURL { get; set; }
+        public string LocalPath { get; set; }
+        public RepoSourceTypeEnum RepoSourceTypeEnum { get; set; }
+    }
+}
---
+++ Views/CloneMultipleForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace RepoManager.Views
+{
+    partial class CloneMultipleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CloneMultipleForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelRepoType = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelLocalPath = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxLocalFolderName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxRemoteUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.dataGridViewReposToClone = new System.Windows.Forms.DataGridView();
+            this.ColumnType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnFolderName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnRemoteURL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReposToClone)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonOK);
+            this.panel1.Controls.Add(this.buttonCancel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 465);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(6);
+            this.panel1.Size = new System.Drawing.Size(742, 40);
+            this.panel1.TabIndex = 5;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonOK.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.buttonOK.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttonOK.Location = new System.Drawing.Point(526, 6);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(105, 28);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonCancel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttonCancel.Location = new System.Drawing.Point(631, 6);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(105, 28);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.labelRepoType);
+            this.groupBox1.Controls.Add(this.labelStatus);
+            this.groupBox1.Controls.Add(this.labelLocalPath);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxLocalFolderName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxRemoteUrl);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(722, 188);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // labelRepoType
+            // 
+            this.labelRepoType.AutoSize = true;
+            this.labelRepoType.Location = new System.Drawing.Point(604, 39);
+            this.labelRepoType.Name = "labelRepoType";
+            this.labelRepoType.Size = new System.Drawing.Size(31, 15);
+            this.labelRepoType.TabIndex = 7;
+            this.labelRepoType.Text = "Type";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(163, 148);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(39, 15);
+            this.labelStatus.TabIndex = 6;
+            this.labelStatus.Text = "Status";
+            // 
+            // labelLocalPath
+            // 
+            this.labelLocalPath.AutoSize = true;
+            this.labelLocalPath.Location = new System.Drawing.Point(163, 115);
+            this.labelLocalPath.Name = "labelLocalPath";
+            this.labelLocalPath.Size = new System.Drawing.Size(31, 15);
+            this.labelLocalPath.TabIndex = 5;
+            this.labelLocalPath.Text = "Path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Local Path";
+            // 
+            // textBoxLocalFolderName
+            // 
+            this.textBoxLocalFolderName.Location = new System.Drawing.Point(163, 69);
+            this.textBoxLocalFolderName.Name = "textBoxLocalFolderName";
+            this.textBoxLocalFolderName.Size = new System.Drawing.Size(191, 23);
+            this.textBoxLocalFolderName.TabIndex = 3;
+            this.textBoxLocalFolderName.TextChanged += new System.EventHandler(this.textBoxLocalFolderName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Local Folder Name";
+            // 
+            // textBoxRemoteUrl
+            // 
+            this.textBoxRemoteUrl.Location = new System.Drawing.Point(163, 36);
+            this.textBoxRemoteUrl.Name = "textBoxRemoteUrl";
+            this.textBoxRemoteUrl.Size = new System.Drawing.Size(433, 23);
+            this.textBoxRemoteUrl.TabIndex = 1;
+            this.textBoxRemoteUrl.TextChanged += new System.EventHandler(this.textBoxRemoteUrl_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Remote URL";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(14, 201);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 7;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(95, 201);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 8;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // dataGridViewReposToClone
+            // 
+            this.dataGridViewReposToClone.AllowUserToAddRows = false;
+            this.dataGridViewReposToClone.AllowUserToDeleteRows = false;
+            this.dataGridViewReposToClone.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewReposToClone.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridViewReposToClone.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewReposToClone.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnType,
+            this.ColumnFolderName,
+            this.ColumnRemoteURL});
+            this.dataGridViewReposToClone.Location = new System.Drawing.Point(14, 230);
+            this.dataGridViewReposToClone.Name = "dataGridViewReposToClone";
+            this.dataGridViewReposToClone.ReadOnly = true;
+            this.dataGridViewReposToClone.Size = new System.Drawing.Size(722, 229);
+            this.dataGridViewReposToClone.TabIndex = 9;
+            // 
+            // ColumnType
+            // 
+            this.ColumnType.HeaderText = "Type";
+            this.ColumnType.Name = "ColumnType";
+            this.ColumnType.ReadOnly = true;
+            this.ColumnType.Width = 56;
+            // 
+            // ColumnFolderName
+            // 
+            this.ColumnFolderName.HeaderText = "Folder Name";
+            this.ColumnFolderName.Name = "ColumnFolderName";
+            this.ColumnFolderName.ReadOnly = true;
+            // 
+            // ColumnRemoteURL
+            // 
+            this.ColumnRemoteURL.HeaderText = "Remote URL";
+            this.ColumnRemoteURL.Name = "ColumnRemoteURL";
+            this.ColumnRemoteURL.ReadOnly = true;
+            this.ColumnRemoteURL.Width = 97;
+            // 
+            // CloneMultipleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(742, 505);
+            this.Controls.Add(this.dataGridViewReposToClone);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CloneMultipleForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clone Multiple Repos";
+            this.Load += new System.EventHandler(this.CloneMultipleForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReposToClone)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelRepoType;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelLocalPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxLocalFolderName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxRemoteUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.DataGridView dataGridViewReposToClone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFolderName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnRemoteURL;
+    }
+}
